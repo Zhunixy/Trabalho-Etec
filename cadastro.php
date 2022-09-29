022e60ed403064dc5f1c13a02450b2450f38bae0
Zhunixy <kauan8643@gmail.com>
2022-09-29 12:27:33 -0300

    Melhorias

--- cadastro.php
+++ cadastro.php
@@ -2,10 +2,16 @@
 
 $usuario = $_POST['usuario'];
 $senha = $_POST['senha'];
+$email = $_POST['email'];
 
 include("login.html");
 include("cadastro.html");
 
-if($usuario == $usuario && $senha == $senha){
-    header("location: login.html");
+if($usuario == null || $senha == null){ 
+    header(location: "cadastro.html");
 }
+
+if($usuario == "adm" && $senha == "adm"){
+    header(location: "login.html");
+}
+
--- login.php
+++ login.php
@@ -2,18 +2,21 @@
 
 $usuario = $_POST['usuario'];
 $senha = $_POST['senha'];
-$email = $_POST['email'];
 
 include("home.html");  //Vai linkar a pagina html com o PHP
 include("cadastro.html");
 
-if($usuario === null || $senha === null){
-    header("location: cadastro.html");
+if($usuario === null && $senha === null){?>
+    <a href="cadastro.html"></a>
+    <?php
+
+}else{ ?>
+    <a href="home.html"></a>
+    
+    <?php
 }
 
-if($usuario == $usuario && $senha == $senha){
-    header("location: home.html");
-}
+
 
 
 
