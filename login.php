1c24477d6c186937841dd8028c646f5d2aa5135c
Zhunixy <kauan8643@gmail.com>
2022-10-22 18:38:20 -0300

    Melhorias nos formularios e mexi um pouco na home

--- login.php
+++ login.php
@@ -1,21 +1,3 @@
-<?php
-
-$usuario = $_POST['usuario'];
-$senha = $_POST['senha'];
-
-include("home.html");  //Vai linkar a pagina html com o PHP
-include("cadastro.html");
-
-if($usuario === null && $senha === null){?>
-    <a href="cadastro.html"></a>
-    <?php
-
-}else{ ?>
-    <a href="home.html"></a>
-    
-    <?php
-}
-
 
 
 
