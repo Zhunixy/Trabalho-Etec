266800d1c145306f554a35f26c1adc2e548500a3
Razawaky <viniciusduarte564@gmail.com>
2022-11-11 08:51:34 -0300

    biblioterca

--- biblioteca/main.js
+++
@@ -1,8 +0,0 @@
-const bntMobile = document.getElementById('bnt-mobile');
-
-function toggleMenu() {
-    const nav = document.getElementById('nav-menu');
-    nav.classList.toggle('active');
-}
-
-bntMobile.addEventListener('click', toggleMenu);
