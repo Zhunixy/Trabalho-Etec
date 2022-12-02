fb69e532a676a4630ade5383b33e109eb710cb81
santaninhaaaa <oliveirasantanavinicius@gmail.com>
2022-12-02 18:52:34 -0300

    alterações

--- bancodedados/php/cadastro.php
+++ bancodedados/php/cadastro.php
@@ -3,13 +3,9 @@
 include_once "conexao.php";
 
 try {
-    $rm = filter_var($_POST['rm']);
     $email = filter_var($_POST['email']);
-    $senha = filter_var($_POST['senha']);
-    $insert = $conectar -> prepare("INSERT INTO aluno (rm, email, senha) VALUES (:rm, :email, :senha)");
-    $insert -> bindParam (':rm', $rm);
+    $insert = $conectar -> prepare("INSERT INTO cadastrar (email) VALUES (:email)");
     $insert -> bindParam (':email', $email);
-    $insert -> bindParam (':senha', $senha);
     $insert -> execute();
     header("location:.../index.html");
 } catch (PDOException $e) {
--- bancodedados/php/conexao.php
+++ bancodedados/php/conexao.php
@@ -1,7 +1,7 @@
 <?php
-try [
-    $conectar = new PDO("mysql:host=localhost;port=3306;dbname=login;", "root", " ");
-] catch (PDOException $e) {
+try {
+    $conectar = new PDO("mysql:host=localhost;port=3306;dbname=pegaremail;","root","");
+} catch (PDOException $e) {
     echo $e -> getMessage();
 }
 ?>
