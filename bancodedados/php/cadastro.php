2dd0ddf7725e0077c5f1b797e682ec55a0239e66
santaninhaaaa <oliveirasantanavinicius@gmail.com>
2022-12-03 16:11:27 -0300

    reformas

--- bancodedados/php/cadastro.php
+++ bancodedados/php/cadastro.php
@@ -3,9 +3,15 @@
 include_once "conexao.php";
 
 try {
+    $nome = filter_var($_POST['nome']);
     $email = filter_var($_POST['email']);
-    $insert = $conectar -> prepare("INSERT INTO cadastrar (email) VALUES (:email)");
+    $assunto = filter_var($_POST['assunto']);
+    $mensagem = filter_var($_POST['mensagem']);
+    $insert = $conectar -> prepare("INSERT INTO pegarinfo (nome, email, assunto, mensagem) VALUES (:nome, :email, :assunto, :mensagem)");
+    $insert -> bindParam (':nome', $nome);
     $insert -> bindParam (':email', $email);
+    $insert -> bindParam (':assunto', $assunto);
+    $insert -> bindParam (':mensagem', $mensagem);
     $insert -> execute();
     header("location:.../index.html");
 } catch (PDOException $e) {
