43bb388cce176a8678c1e5d924a88f1b3e1af3cd
santaninhaaaa <oliveirasantanavinicius@gmail.com>
2022-12-04 19:56:30 -0300

    adição do php e bd

--- bancodedados/php/cadastro.php
+++ bancodedados/php/cadastro.php
@@ -3,17 +3,11 @@
 include_once "conexao.php";
 
 try {
-    $nome = filter_var($_POST['nome']);
     $email = filter_var($_POST['email']);
-    $assunto = filter_var($_POST['assunto']);
-    $mensagem = filter_var($_POST['mensagem']);
-    $insert = $conectar -> prepare("INSERT INTO pegarinfo (nome, email, assunto, mensagem) VALUES (:nome, :email, :assunto, :mensagem)");
-    $insert -> bindParam (':nome', $nome);
+    $insert = $conectar -> prepare("INSERT INTO cadastrar (email) VALUES (:email)");
     $insert -> bindParam (':email', $email);
-    $insert -> bindParam (':assunto', $assunto);
-    $insert -> bindParam (':mensagem', $mensagem);
     $insert -> execute();
-    header("location:.../index.html");
+    header("location: .../index.html");
 } catch (PDOException $e) {
     echo $e -> getMessage();
 }
--- bancodedados/php/conexao.php
+++ bancodedados/php/conexao.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $conectar = new PDO("mysql:host=localhost;port=3306;dbname=info;","root","");
+    $conectar = new PDO("mysql:host=localhost;port=3306;dbname=pegaremail;","root","");
 } catch (PDOException $e) {
     echo $e -> getMessage();
 }
